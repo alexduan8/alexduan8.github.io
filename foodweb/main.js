5f616e25a5258be333928637b54ef3f9185ac00d
alexduan8 <58568755+alexduan8@users.noreply.github.com>
2020-04-26 22:49:15 -0700

    Add files via upload

---
+++ foodweb/main.js
@@ -0,0 +1,326 @@
+
+const theSun = document.querySelector("#sun");
+const target = document.querySelector("#target");
+const target2 = document.querySelector("#target2");
+const target3 = document.querySelector("#target3");
+const target4 = document.querySelector("#target4");
+const target5 = document.querySelector("#target5");
+const target6 = document.querySelector("#target6");
+const target7 = document.querySelector("#target7");
+const target8 = document.querySelector("#target8");
+const target9 = document.querySelector("#target9");
+const target10 = document.querySelector("#target10");
+const target11 = document.querySelector("#target11");
+const target12 = document.querySelector("#target12");
+const target13 = document.querySelector("#target13");
+const target14 = document.querySelector("#target14");
+const target15 = document.querySelector("#target15");
+const plant1 = document.querySelector("#plant_description");
+const plant2 = document.querySelector("#plant_description2");
+const plant3 = document.querySelector("#plant_description3");
+const plant4 = document.querySelector("#plant_description4");
+const plant5 = document.querySelector("#plant_description5");
+const plant6 = document.querySelector("#plant_description6");
+const plant7 = document.querySelector("#plant_description7");
+const plant8 = document.querySelector("#plant_description8");
+const plant9 = document.querySelector("#plant_description9");
+const plant10 = document.querySelector("#plant_description10");
+const plant11 = document.querySelector("#plant_description11");
+const plant12 = document.querySelector("#plant_description12");
+const plant13 = document.querySelector("#plant_description13");
+const plant14 = document.querySelector("#plant_description14");
+const plant15 = document.querySelector("#plant_description15");
+const arrowtest = document.querySelector("#arrowtest");
+const arrow1 = document.querySelector("#arrow1");
+const arrow2 = document.querySelector("#arrow2");
+const arrow3 = document.querySelector("#arrow3");
+const arrow4 = document.querySelector("#arrow4");
+const arrow5 = document.querySelector("#arrow5");
+const arrow6 = document.querySelector("#arrow6");
+const arrow7 = document.querySelector("#arrow7");
+const arrow8 = document.querySelector("#arrow8");
+const arrow9 = document.querySelector("#arrow9");
+const arrow10 = document.querySelector("#arrow10");
+const arrow11 = document.querySelector("#arrow11");
+const arrow12 = document.querySelector("#arrow12");
+const arrow13 = document.querySelector("#arrow13");
+const arrow14 = document.querySelector("#arrow14");
+const arrow15 = document.querySelector("#arrow15");
+const arrow16 = document.querySelector("#arrow16");
+const arrow17 = document.querySelector("#arrow17");
+const arrow18 = document.querySelector("#arrow18");
+const arrow19 = document.querySelector("#arrow19");
+const arrow20 = document.querySelector("#arrow20");
+const arrow21 = document.querySelector("#arrow21");
+const arrow22 = document.querySelector("#arrow22");
+const arrow23 = document.querySelector("#arrow23");
+const arrow24 = document.querySelector("#arrow24");
+const arrow25 = document.querySelector("#arrow25");
+const arrow26 = document.querySelector("#arrow26");
+const arrow27 = document.querySelector("#arrow27");
+//const deer = document.querySelector("#deer");
+//target.addEventListener('mouseover', hoverOn);
+
+
+//function hoverOn(){
+    //console.log("Check");
+    //target.style.opacity = "100%";
+    //target.fadeIn(500); 
+//}
+
+//hovers
+$( "#sun" ).first().mouseover(function() {
+  $(this).fadeTo( "fast", 1 );
+  arrow1.style.opacity = "100%";
+  arrow2.style.opacity = "100%";
+  //arrow3.style.opacity = "100%";
+  //arrow4.style.opacity = "100%";
+  //arrow5.style.opacity = "100%";
+  arrow26.style.opacity = "100%";
+  arrow27.style.opacity = "100%";
+  console.log("hovered");
+});
+
+$( "#target" ).first().mouseover(function() {
+  $(this).fadeTo( "fast", 1 );
+  target.style.opacity = "100%";
+  arrow6.style.opacity = "100%";
+  console.log("hovered");
+});
+
+$( "#target2" ).first().mouseover(function() {
+  $(this).fadeTo( "fast", 1 );
+  target2.style.opacity = "100%";
+  arrow14.style.opacity = "100%";
+  console.log("hovered");
+});
+
+$( "#target3" ).first().mouseover(function() {
+  $(this).fadeTo( "fast", 1 );
+  target3.style.opacity = "100%";
+  //arrowtest.style.opacity = "100%";
+  //arrow7.style.opacity = "100%";
+  //arrow8.style.opacity = "100%";
+  //arrow9.style.opacity = "100%";
+  arrow25.style.opacity = "100%"
+  console.log("hovered");
+});
+
+$( "#target4" ).first().mouseover(function() {
+  $(this).fadeTo( "fast", 1 );
+  target4.style.opacity = "100%";
+  arrow10.style.opacity = "100%";
+  //arrow11.style.opacity = "100%";
+  //arrow12.style.opacity = "100%";
+  //arrow13.style.opacity = "100%";
+  console.log("hovered");
+});
+
+$( "#target5" ).first().mouseover(function() {
+  $(this).fadeTo( "fast", 1 );
+  target5.style.opacity = "100%";
+  arrow15.style.opacity = "100%";
+  //arrow16.style.opacity = "100%";
+  //arrow17.style.opacity = "100%";
+  console.log("hovered");
+});
+
+$( "#target6" ).first().mouseover(function() {
+  $(this).fadeTo( "fast", 1 );
+  target6.style.opacity = "100%";
+  arrow18.style.opacity = "100%";
+  console.log("hovered");
+});
+
+$( "#target7" ).first().mouseover(function() {
+  $(this).fadeTo( "fast", 1 );
+  target7.style.opacity = "100%";
+  arrow19.style.opacity = "100%";
+  console.log("hovered");
+});
+
+$( "#target8" ).first().mouseover(function() {
+  $(this).fadeTo( "fast", 1 );
+  target8.style.opacity = "100%";
+  arrow20.style.opacity = "100%";
+  console.log("hovered");
+});
+
+$( "#target9" ).first().mouseover(function() {
+  $(this).fadeTo( "fast", 1 );
+  target9.style.opacity = "100%";
+  arrow21.style.opacity = "100%";
+  arrow22.style.opacity = "100%";
+  console.log("hovered");
+});
+
+$( "#target10" ).first().mouseover(function() {
+  $(this).fadeTo( "fast", 1 );
+  target10.style.opacity = "100%";
+  // arrow1.style.opacity = "100%";
+  console.log("hovered");
+});
+
+$( "#target11" ).first().mouseover(function() {
+  $(this).fadeTo( "fast", 1 );
+  target11.style.opacity = "100%";
+  console.log("hovered");
+});
+
+$( "#target12" ).first().mouseover(function() {
+  $(this).fadeTo( "fast", 1 );
+  target12.style.opacity = "100%";
+  arrow23.style.opacity = "100%";
+  console.log("hovered");
+});
+
+$( "#target13" ).first().mouseover(function() {
+  $(this).fadeTo( "fast", 1 );
+  target13.style.opacity = "100%";
+  // arrow1.style.opacity = "100%";
+  console.log("hovered");
+});
+
+$( "#target14" ).first().mouseover(function() {
+  $(this).fadeTo( "fast", 1 );
+  target14.style.opacity = "100%";
+  arrow24.style.opacity = "100%";
+  console.log("hovered");
+});
+
+$( "#target15" ).first().mouseover(function() {
+  $(this).fadeTo( "fast", 1 );
+  target15.style.opacity = "100%";
+  // arrow1.style.opacity = "100%";
+  console.log("hovered");
+});
+
+
+
+//clicks
+$("#target").click(function(){
+ console.log("clickedfern");
+ plant1.style.opacity = "100%";
+});
+
+$("#target2").click(function(){
+  console.log("clickedcashew");
+  plant2.style.opacity = "100%";
+ });
+
+ $("#target3").click(function(){
+  console.log("clickeddeer");
+  plant3.style.opacity = "100%";
+ });
+
+ $("#target4").click(function(){
+   console.log("clickedfrog");
+   plant4.style.opacity = "100%";
+  });
+
+  $("#target5").click(function(){
+    console.log("clickedlemur");
+    plant5.style.opacity = "100%";
+   });
+
+   $("#target6").click(function(){
+    console.log("clickedpine");
+    plant6.style.opacity = "100%";
+   });
+
+   $("#target7").click(function(){
+     console.log("clickedsquirrel");
+     plant7.style.opacity = "100%";
+    });
+
+    $("#target8").click(function(){
+     console.log("clickedberries");
+     plant8.style.opacity = "100%";
+    });
+
+    $("#target9").click(function(){
+      console.log("clickedbutterfly");
+      plant9.style.opacity = "100%";
+     });
+
+     $("#target10").click(function(){
+       console.log("clickedbobcat");
+       plant10.style.opacity = "100%";
+      });
+
+      $("#target11").click(function(){
+        console.log("clickedcoyote");
+        plant11.style.opacity = "100%";
+       });
+
+       $("#target12").click(function(){
+         console.log("clickedlizard");
+         plant12.style.opacity = "100%";
+        });
+
+        $("#target13").click(function(){
+         console.log("clickedraven");
+         plant13.style.opacity = "100%";
+        });
+
+        $("#target14").click(function(){
+          console.log("clickedpinemarten");
+          plant14.style.opacity = "100%";
+         });
+
+         $("#target15").click(function(){
+           console.log("clickedlion");
+           plant15.style.opacity = "100%";
+          });
+//spacebar
+$('body').keyup(function(e){
+   if(e.keyCode == 32){
+       // user has pressed space
+       arrow1.style.opacity = "0%";
+       arrow2.style.opacity = "0%";
+       arrow26.style.opacity = "0%";
+       arrow27.style.opacity = "0%";
+     target.style.opacity = "0%";
+       arrow6.style.opacity = "0%";
+     target2.style.opacity = "0%";
+       arrow14.style.opacity = "0%";
+     target3.style.opacity = "0%";
+     arrow25.style.opacity = "0%"
+     target4.style.opacity = "0%";
+       arrow10.style.opacity = "0%";
+     target5.style.opacity = "0%";
+       arrow15.style.opacity = "0%";
+     target6.style.opacity = "0%";
+       arrow18.style.opacity = "0%";
+     target7.style.opacity = "0%";
+       arrow19.style.opacity = "0%";
+     target8.style.opacity = "0%";
+       arrow20.style.opacity = "0%";
+     target9.style.opacity = "0%";
+       arrow21.style.opacity = "0%";
+       arrow22.style.opacity = "0%";
+     target10.style.opacity = "0%";
+     target11.style.opacity = "0%";
+     target12.style.opacity = "0%";
+       arrow23.style.opacity = "0%";
+     target13.style.opacity = "0%";
+     target14.style.opacity = "0%";
+       arrow24.style.opacity = "0%";
+     target15.style.opacity = "0%";
+     plant1.style.opacity = "0%";
+     plant2.style.opacity = "0%";
+     plant3.style.opacity = "0%";
+     plant4.style.opacity = "0%";
+     plant5.style.opacity = "0%";
+     plant6.style.opacity = "0%";
+     plant7.style.opacity = "0%";
+     plant8.style.opacity = "0%";
+     plant9.style.opacity = "0%";
+     plant10.style.opacity = "0%";
+     plant11.style.opacity = "0%";
+     plant12.style.opacity = "0%";
+     plant13.style.opacity = "0%";
+     plant14.style.opacity = "0%";
+     plant15.style.opacity = "0%";
+   }
+});
